107346a64f64ed34d60c24052ce989f19a901582
unknown <rohaan.shaikh129@gmail.com>
2025-10-19 13:29:54 +0500

    folder structure

--- lib/feature/on_boarding/walk_through/walk_through.dart
+++ lib/feature/on_boarding/walk_through/walk_through.dart
@@ -1,5 +1,5 @@
 import 'package:ast_official/app_ui_helpers/app_router/app_router.dart';
-import 'package:ast_official/feature/on_boarding/select_plan/select_plan.dart';
+import 'package:ast_official/feature/on_boarding/select_plan/select_plan_view.dart';
 import 'package:ast_official/helpers/app_layout_helper.dart';
 import 'package:ast_official/ui_molecules/app_text/app_text.dart';
 import 'package:ast_official/ui_molecules/buttons/app_primary_button.dart';
